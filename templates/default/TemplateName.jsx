ab37a3d6eaa3c36d46ed5ae22022282009a37486
Grimlerih <kanatuulud@gmail.com>
2023-09-26 14:07:01 +0600

    build: добавлен grc

---
+++ templates/default/TemplateName.jsx
@@ -0,0 +1,9 @@
+import styles from "./templateName.module.scss";
+
+export const TemplateName = () => {
+  return (
+    <>
+      <div className={styles.templateName}>TemplateName</div>
+    </>
+  );
+};
