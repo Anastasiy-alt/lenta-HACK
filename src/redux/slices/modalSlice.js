bc0197e54e8508df4c3f4d1e7ba0357fb2f928b3
Grimlerih <kanatuulud@gmail.com>
2023-09-28 21:53:17 +0600

    feat: слайс для модального окна

---
+++ src/redux/slices/modalSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isOpen: true,
+};
+
+const modalSlice = createSlice({
+  name: "modal",
+  initialState,
+  reducers: {
+    modalOpen(state) {
+      state.isOpen = true;
+    },
+    modalClose(state) {
+      state.isOpen = false;
+    },
+  },
+});
+
+export const { modalOpen, modalClose } = modalSlice.actions;
+export default modalSlice.reducer;
