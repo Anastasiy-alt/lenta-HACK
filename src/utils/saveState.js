940b1331a407a25459307314ab798ccb046ed8d0
Grimlerih <kanatuulud@gmail.com>
2023-10-03 19:43:17 +0600

    feat: сохранение sessionStorage

---
+++ src/utils/saveState.js
@@ -0,0 +1,23 @@
+// Сохранение состояния
+export const saveState = (state, stateName) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    sessionStorage.setItem(stateName, serializedState);
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+// Загрузка состояния
+export const loadState = (stateName) => {
+  try {
+    const serializedState = sessionStorage.getItem(stateName);
+    if (serializedState === null) {
+      return undefined;
+    }
+    return JSON.parse(serializedState);
+  } catch (e) {
+    console.log(e);
+    return undefined;
+  }
+};
