9a0dbd1821cd24ec548b32273b9d4341c9c523a2
Grimlerih <kanatuulud@gmail.com>
2023-10-05 20:56:47 +0600

    feat: динамическое отображение категорий

--- src/components/FilterProductCategories/FilterProductCategories.jsx
+++ src/components/FilterProductCategories/FilterProductCategories.jsx
@@ -3,47 +3,21 @@ import styles from "./FilterProductCategories.module.scss";
 import { ProductCategories } from "../ProductCategories/ProductCategories";
 import { useDispatch, useSelector } from "react-redux";
 import { modalOpen } from "../../redux/slices/modalSlice.js";
-
-const cookingLenta = [
-  "Гриль",
-  "Порционные блюда",
-  "Соусы",
-  "Гарниры",
-  "Горячие блюда",
-  "Корейские салаты",
-  "Соленья",
-  "Порционные салаты",
-  "Мучные продукты, десерты",
-  "Салаты и закуски",
-];
-
-const bakery = [
-  "Пончики, кексы, шоколад",
-  "Пироги",
-  "Мелкоштучные изделия",
-  "Национальные изделия",
-];
-
-const bread = [
-  "Багеты и булочки",
-  "Ржаной, ржано-пшеничный, пшеничный",
-  "Бездрожжевой хлеб",
-  "Хлеб с добавками",
-  "Национальные хлеба",
-];
-
-const drinks = ["Холодные напитки"];
-
-const fastFood = ["Сэндвичи, бутерброды, роллы в лаваше", "Фритюр"];
+import { nanoid } from "nanoid";
 
 export const FilterProductCategories = () => {
   const { isOpen } = useSelector((store) => store.modal);
+  const { categories, uniqueGroup, group } = useSelector(
+    (store) => store.categories
+  );
   const dispatch = useDispatch();
   //для тест кнопки закрытия
   const closePopup = () => {
     dispatch(modalOpen());
   };
-
+  console.log(uniqueGroup);
+  console.log(categories);
+  console.log(group);
   const [resetAll, setResetAll] = useState(false);
 
   const resetAllCategories = () => {
@@ -54,64 +28,44 @@ export const FilterProductCategories = () => {
     <section
       className={`${styles.categories} ${isOpen && styles.categories_open}`}
     >
-      <header className={styles.categories__header}>
+      <div className={styles.categories__header}>
         <h1 className={styles.categories__title}>Выбери разделы СП</h1>
-        <button
-          className={styles.categories__reset}
-          onClick={resetAllCategories}
-        >
-          Сбросить выбор
-        </button>
+
         <button
           className={styles.categories__close}
           onClick={closePopup}
         ></button>
-      </header>
+      </div>
 
       <article className={styles.categories__listBlock}>
-        <div>
+        {Object.entries(group).map(([group, categories]) => (
+          <div className={styles.categories__item} key={nanoid(6)}>
+            <ProductCategories
+              categoryTitle={group}
+              categoryTitleEnglish="CookingLenta"
+              categoryItems={categories}
+              resetAll={resetAll}
+              setResetAll={setResetAll}
+            />
+          </div>
+        ))}
+        {/* <div className={styles.categories__item}>
           <ProductCategories
-            categoryTitle='Кулинария "Лента"'
+            key={nanoid(6)}
+            categoryTitle={"Продукция быстрого питания «Лента»"}
             categoryTitleEnglish="CookingLenta"
-            categoryItems={cookingLenta}
+            categoryItems={["dqwdqdq", "dqwdqdq", "dqwdqdq", "dqwdqdq"]}
             resetAll={resetAll}
             setResetAll={setResetAll}
           />
-        </div>
-
-        <div>
-          <ProductCategories
-            categoryTitle="Выпечка"
-            categoryTitleEnglish="Bakery"
-            categoryItems={bakery}
-            resetAll={resetAll}
-            setResetAll={setResetAll}
-          />
-          <ProductCategories
-            categoryTitle="Хлеб"
-            categoryTitleEnglish="Bread"
-            categoryItems={bread}
-            resetAll={resetAll}
-            setResetAll={setResetAll}
-          />
-        </div>
-
-        <div>
-          <ProductCategories
-            categoryTitle="Напитки"
-            categoryTitleEnglish="Drinks"
-            categoryItems={drinks}
-            resetAll={resetAll}
-            setResetAll={setResetAll}
-          />
-
-          <ProductCategories
-            categoryTitle="Продукция быстрого питания"
-            categoryTitleEnglish="FastFood"
-            categoryItems={fastFood}
-            resetAll={resetAll}
-            setResetAll={setResetAll}
-          />
+        </div> */}
+        <div className={styles.btn_container}>
+          <button
+            className={styles.categories__reset}
+            onClick={resetAllCategories}
+          >
+            Сбросить выбор
+          </button>
           <button className={styles.categories__button}>
             Показать результат
           </button>
