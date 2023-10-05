012b3d153f1ac728fa3c04e158f69d70f20ae99d
Grimlerih <kanatuulud@gmail.com>
2023-10-05 20:53:57 +0600

    feat:  логика проверки ответа в отдельный файл

--- src/services/API/shopApi.js
+++ src/services/API/shopApi.js
@@ -1,17 +1,6 @@
-// import { request } from "./userApi";
+import { request } from "../checkResponse";
 
 const url = "http://94.131.100.195/api/v1/";
-
-const checkResponse = async (res) => {
-  if (res.ok) {
-    return res.json();
-  }
-  return Promise.reject(`Ошибка ${res.status}`);
-};
-
-export const request = (url, options) => {
-  return fetch(url, options).then(checkResponse);
-};
 
 export const getCityApi = () => {
   return request(`${url}shops/`);
