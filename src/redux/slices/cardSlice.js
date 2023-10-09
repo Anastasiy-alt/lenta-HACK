bdec0bae322fbf9e82e72dc87d543f476f315e38
Grimlerih <kanatuulud@gmail.com>
2023-10-09 12:25:40 +0600

    feat: запрос данных диаграммы

--- src/redux/slices/cardSlice.js
+++ src/redux/slices/cardSlice.js
@@ -1,7 +1,41 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getStatisicApi, getForecastApi } from "../../services/API/cardApi";
+
+export const getStatisticData = createAsyncThunk(
+  "card/getStatisticData",
+  async function ({ currentCardName, currentStoreArr }, { rejectWithValue }) {
+    try {
+      const response = await getStatisicApi({
+        currentCardName,
+        currentStoreArr,
+      });
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getForecastData = createAsyncThunk(
+  "card/getForecastData",
+  async function ({ currentCardName, currentStoreArr }, { rejectWithValue }) {
+    try {
+      const response = await getForecastApi({
+        currentCardName,
+        currentStoreArr,
+      });
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.message);
+    }
+  }
+);
 
 const initialState = {
   cardList: [],
+  currentCardName: "",
+  statisticData: [],
+  forecastData: [],
 };
 
 const cardSlice = createSlice({
@@ -9,10 +43,36 @@ const cardSlice = createSlice({
   initialState,
   reducers: {
     setCardList(state, action) {
+      state.cardList = [];
       state.cardList = action.payload;
+    },
+    setCardName(state, action) {
+      state.currentCardName = "";
+      state.currentCardName = action.payload;
+    },
+  },
+  extraReducers: {
+    [getStatisticData.pending]: () => {},
+    [getForecastData.pending]: () => {},
+
+    [getForecastData.fulfilled]: (state, action) => {
+      state.forecastData = [];
+      state.forecastData = action.payload;
+      console.log(action.payload);
+    },
+    [getStatisticData.fulfilled]: (state, action) => {
+      state.statisticData = [];
+      state.statisticData = action.payload;
+      console.log(action.payload);
+    },
+    [getForecastData.rejected]: () => {
+      console.log("rejected");
+    },
+    [getStatisticData.rejected]: () => {
+      console.log("rejected");
     },
   },
 });
 
-export const { setCardList } = cardSlice.actions;
+export const { setCardList, setCardName } = cardSlice.actions;
 export default cardSlice.reducer;
